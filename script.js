c5efcc76c5babf481f037a37ed4d33f8486de7d8
SamriddhiMaurya <samriddimaurya9005@gmail.com>
2024-05-15 21:30:55 +0530

    new api change commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "73d73599a8b8488d9d5469e000f635cf";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
